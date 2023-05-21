ed061ef80558848c2dc237eae030edd6b945be58
Ravi Sharma <ravisharma1987@gmail.com>
2023-05-21 10:28:25 +0200

    new pages

---
+++ pages/impressum.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>privaty policy</h1>
+</template>
+
+  <script>
+export default {
+  name: 'IndexPage',
+}
+  </script>
---
+++ pages/my-academic-life.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>musings-on-east-asian-buddhism</h1>
+</template>
+
+    <script>
+export default {
+  name: 'IndexPage',
+}
+    </script>
